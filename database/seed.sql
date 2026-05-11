a5b83b424d1776feae09510d27c5a98dc3995c7b
Ivana <kivanak@gmail.com>
2026-05-11 17:41:23 +0200

    Added UCG faculties seed data and working API

---
+++ database/seed.sql
@@ -0,0 +1,41 @@
+INSERT INTO faculties
+(name, university_name, city, address, type, description, website_url)
+VALUES
+
+('Arhitektonski fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Bulevar Džordža Vašingtona bb', 'drzavni', 'Studije arhitekture, urbanizma i prostornog planiranja.', 'https://www.ucg.ac.me/af'),
+
+('Biotehnički fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Mihaila Lalića 1', 'drzavni', 'Istraživanja i studije u oblasti poljoprivrede, veterine i hrane.', 'https://www.ucg.ac.me/btf'),
+
+('Ekonomski fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Jovana Tomaševića 37', 'drzavni', 'Obrazovanje u sferama ekonomije, menadžmenta i biznisa.', 'https://www.ucg.ac.me/ef'),
+
+('Elektrotehnički fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Džordža Vašingtona bb', 'drzavni', 'Vodeća institucija za ICT, energetiku i automatiku.', 'https://www.ucg.ac.me/etf'),
+
+('Fakultet dramskih umjetnosti', 'Univerzitet Crne Gore', 'Cetinje', 'Bajova 5', 'drzavni', 'Obrazovanje kadrova za pozorište, film, radio i televiziju.', 'https://www.ucg.ac.me/fdu'),
+
+('Fakultet likovnih umjetnosti', 'Univerzitet Crne Gore', 'Cetinje', 'Njegoševa bb', 'drzavni', 'Visokoškolska ustanova za slikarstvo, vajarstvo i grafički dizajn.', 'https://www.ucg.ac.me/flu'),
+
+('Fakultet političkih nauka', 'Univerzitet Crne Gore', 'Podgorica', '13. jula br. 2', 'drzavni', 'Studije politikologije, novinarstva, socijalne politike i diplomatije.', 'https://www.ucg.ac.me/fpn'),
+
+('Fakultet za pomorstvo', 'Univerzitet Crne Gore', 'Kotor', 'Put I Bokeljske brigade 44', 'drzavni', 'Obrazovanje visokostručnih kadrova za pomorsku privredu.', 'https://www.ucg.ac.me/fzp'),
+
+('Fakultet za sport i fizičko vaspitanje', 'Univerzitet Crne Gore', 'Nikšić', 'Narodnih heroja bb', 'drzavni', 'Naučno-istraživački rad u oblasti sporta i fizičke kulture.', 'https://www.ucg.ac.me/sport'),
+
+('Fakultet za turizam i hotelijerstvo', 'Univerzitet Crne Gore', 'Kotor', 'Stari grad 320', 'drzavni', 'Specijalizovana ustanova za menadžment u turizmu i hotelijerstvu.', 'https://www.ucg.ac.me/fth'),
+
+('Filološki fakultet', 'Univerzitet Crne Gore', 'Nikšić', 'Danila Bojovića bb', 'drzavni', 'Proučavanje domaćih i stranih jezika i književnosti.', 'https://www.ucg.ac.me/flf'),
+
+('Filozofski fakultet', 'Univerzitet Crne Gore', 'Nikšić', 'Danila Bojovića bb', 'drzavni', 'Obrazovanje u oblasti istorije, geografije, pedagogije i psihologije.', 'https://www.ucg.ac.me/ff'),
+
+('Građevinski fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Džordža Vašingtona bb', 'drzavni', 'Studije građevinarstva i menadžmenta u građevinarstvu.', 'https://www.ucg.ac.me/gf'),
+
+('Mašinski fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Džordža Vašingtona bb', 'drzavni', 'Obrazovanje inženjera mašinstva i drumskog saobraćaja.', 'https://www.ucg.ac.me/mf'),
+
+('Medicinski fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Ljubljanska bb', 'drzavni', 'Studije medicine, farmacije, stomatologije i visoke medicinske škole.', 'https://www.ucg.ac.me/med'),
+
+('Metalurško-tehnološki fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Džordža Vašingtona bb', 'drzavni', 'Inženjerske studije hemijske tehnologije, metalurgije i materijala.', 'https://www.ucg.ac.me/mtf'),
+
+('Muzička akademija', 'Univerzitet Crne Gore', 'Cetinje', 'Njegoševa bb', 'drzavni', 'Umjetnički rad i studije izvođačkih umjetnosti i muzikologije.', 'https://www.ucg.ac.me/ma'),
+
+('Pravni fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Bulevar Revolucije 19', 'drzavni', 'Studije pravnih nauka i pravosudni smjer.', 'https://www.ucg.ac.me/pf'),
+
+('Prirodno-matematički fakultet', 'Univerzitet Crne Gore', 'Podgorica', 'Džordža Vašingtona bb', 'drzavni', 'Studije matematike, računarskih nauka, fizike i biologije.', 'https://www.ucg.ac.me/pmf');
